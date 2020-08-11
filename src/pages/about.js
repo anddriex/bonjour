ce66e37a4a2aba61d3d13732d629258cec654fdc
andresperz <andres@tinkin.one>
2020-08-10 20:46:48 -0500

    added changes

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header"
+
+const About = () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText={"About Gatsby"} />
+    <Header headerText="it's pretty cool" />
+
+    <p>Such wow. Very React.</p>
+  </div>
+)
+
+export default About
